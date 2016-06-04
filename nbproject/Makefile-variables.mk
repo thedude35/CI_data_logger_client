51f1e116709dfad6b547a976d79a01f190d5532e
Devin C <Devin C>
2016-06-04 17:25:29 -0400

    All the project files and configurations for development

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Embedded_Web_Server.X.lib
+CND_ARTIFACT_PATH_default=dist/default/production/Embedded_Web_Server.X.lib
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=EmbeddedWebServer.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/EmbeddedWebServer.X.tar
---
+++ system_config.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   system_config.h
+ * Author: Devin
+ *
+ * Created on June 4, 2016, 4:09 PM
+ */
+
+#ifndef SYSTEM_CONFIG_H
+#define	SYSTEM_CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SYSTEM_CONFIG_H */
+
