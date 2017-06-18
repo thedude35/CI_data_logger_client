63a058964f2caac2aa76fdb75a315738463d1a96
Devin C <Devin C>
2017-06-17 23:18:31 -0400

    removed old microchip platform related files

--- build/default/production/_ext/426468601/TimeDelay.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/426468601/TimeDelay.o:  \
-	C:/microchip_solutions_v2013-06-15/Microchip/Common/TimeDelay.c  \
-	C:/microchip_solutions_v2013-06-15/Microchip/Include/TimeDelay.h  \
-	C:/microchip_solutions_v2013-06-15/Microchip/Include/Compiler.h  \
-	C:/Users/Devin/MPLABXProjects/Embedded_Web_Server.X/system_config/HardwareProfile.h  \
-	C:/microchip_solutions_v2013-06-15/Microchip/Include/GenericTypeDefs.h  \
-
